e2f6ee6acb501434eba1ee7d2770c06e9531fafd
CxLos <bautistac_29@outlook.com>
2025-08-07 23:16:20 -0500

    styling update

--- kidney_disease.py
+++ kidney_disease.py
@@ -453,10 +453,6 @@ df_table.update_layout(
 
 # ============================== Dash Application ========================== #
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
 app = dash.Dash(__name__)
 server = app.server
 
